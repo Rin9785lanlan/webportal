e4a29d0bb15d55e25d88a8c11120e2fb5df9ebdb
s20203053 <s20203053@HCS21039.stdlan.hcs.ac.jp>
2022-06-23 10:00:11 +0900

    pom.xml 追記

--- src/main/java/jp/ac/hcs/s3a313/user/UserController.java
+++ src/main/java/jp/ac/hcs/s3a313/user/UserController.java
@@ -1,9 +1,15 @@
 package jp.ac.hcs.s3a313.user;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -12,14 +18,16 @@ public class UserController {
 
 	/**
 	 * [管理者・上位のみ]ユーザ管理画面への遷移を実施します
-	 * <p>本機能は、ユーザ管理機能の一覧機能を提供します
-	 * <p><strong>この機能は管理者もしくは上位ロールのユーザのみが利用できます</strong>
+	 * <p>
+	 * 本機能は、ユーザ管理機能の一覧機能を提供します
+	 * <p>
+	 * <strong>この機能は管理者もしくは上位ロールのユーザのみが利用できます</strong>
 	 *
 	 * @param model Viewに値を渡すオブジェクト(null不可)
 	 * @return ユーザ管理画面へのパス(null不可)
 	 */
 	@GetMapping("/user/list")
-	public String getUserList(Model model) {
+	public String getUserList(UserForm userForm, Model model) {
 
 		// 結果を取得
 		UserEntity userEntity = userService.selectAll();
@@ -28,23 +36,33 @@ public class UserController {
 
 		return "user/list";
 	}
-	
+
 	/**
 	 * [管理者・上位のみ]ユーザ追加画面を表示します
-	 * <p>本機能は、ユーザ追加機能の一覧機能を提供します
-	 * <p><strong>この機能は管理者もしくは上位ロールのユーザのみが利用できます</strong>
+	 * <p>
+	 * 本機能は、ユーザ追加機能の一覧機能を提供します
+	 * <p>
+	 * <strong>この機能は管理者もしくは上位ロールのユーザのみが利用できます</strong>
 	 *
 	 * @param model Viewに値を渡すオブジェクト(null不可)
 	 * @return ユーザ管理画面へのパス(null不可)
 	 */
 	@GetMapping("/user/insert")
 	public String getUserCreate(Model model) {
-
-		// 前回の入力内容を設定
-		model.addAttribute("userForm", userForm);
-
-		return "user/insert";
 	}
-	
-	
+
+	@PostMapping("/user/insert")
+	public String createUser(@ModelAttribute @Validated UserForm form, BindingResult bindResult, Principal principal,
+			Model model) {
+		if (bindResult.hasErrors()) {
+			return getUserCreate(form, model);
+		}
+		boolean isSuceess = userService.insertOne(form);
+		if (isSuceess) {
+			model.addAttribute("message", "ユーザを登録しました");
+		} else {
+			model.addAttribute("errorMessage", "ユーザ登録に失敗しました。操作をやり直してください");
+		}
+		return getUserList(model);
+	}
 }
---
+++ src/main/java/jp/ac/hcs/s3a313/user/UserForm.java
@@ -0,0 +1,29 @@
+package jp.ac.hcs.s3a313.user;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+/**
+ * ユーザ作成画面の入力値を保持するクラスです
+ * <p>
+ * 各項目のデータ使用は基本設計書を参照してください
+ * 
+ * @author s20203053
+ *
+ */
+@Data
+public class UserForm {
+	/** ユーザID （メールアドレス） */
+	@NotBlank(message = "{require_chaek}")
+	@Email(message = "{email_cheak}")
+	private String userId;
+
+	/** ユーザID （メールアドレス） */
+	@NotBlank(message = "{require_chaek}")
+	@Length(min=6,max=100,message="{length_check}")
+	private String password;
+}
